fd984586d9f570498e5595710a12c2698695b05f
Salnikov Alex <salnikov17a@gmail.com>
2024-09-11 00:27:46 +0300

    created roads and response templates

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,55 @@
 from fastapi import FastAPI
+import models
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def hello():
-    return {"answer": "Hello, world!"}
+@app.get("/author/{id}")
+async def get_author(id: int):
+    return {
+        "id": id,
+        "name": "Alex",
+        "description": "Young guy with broken heart",
+        "keywords": "Simple worlds and teen slang",
+        "specialization": "programming and music",
+        "tone": "not official"
+    }
+
+@app.post("/author")
+async def create_author(author: models.Author):
+    return {
+        "status": "OK",
+        "id": 31
+    }
+
+@app.put("/author")
+async def put_author(author: models.Author):
+    return {
+        "status": "OK",
+        "id": 31
+    }
+
+@app.delete("/author")
+async def delete_author(author: models.Author):
+    return {
+        "status": "OK"
+    }
 
 
+@app.get("/trends")
+async def get_trends():
+    return ["Economic", "Politic", "Sport"]
+
+
+@app.get("/themes/{trend}")
+async def get_themes(trend):
+    return ["Top the most popular sport in 2024", "How to start play basketball"]
+
+
+@app.get("/article")
+async def create_article(authorId: int, theme: str, min_len: int):
+    return {
+        "text": "askfhsdlfshfslsjsfksf;jfpqw idofujewpfd pwufwpofuwp owfowdfjwpo wdufpwdfowufpwfuw pfwdfw-fwfwofiw wdif-wdifwd-f0wifw",
+        "pipelines": ["google.com", "yandex.ru"]
+    }
